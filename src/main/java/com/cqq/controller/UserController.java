623ae9508cb1541db63d770cdae8cd2247b28cf4
Niko <131374852+Hangzhouniko@users.noreply.github.com>
2023-05-03 10:25:30 +0800

    pull test
    pull test

--- src/main/java/com/cqq/controller/UserController.java
+++ src/main/java/com/cqq/controller/UserController.java
@@ -22,6 +22,9 @@ public class UserController {
 
         System.out.println("hot-fix2 test~~");
         System.out.println("push test~~");
+        
+        System.out.println("pull test~~");
+        System.out.println("pull test~~");
 
     }
 }
