7d1e102c36ba85aee0c1240aa7f235cd8f765bff
NiKooo <739812349@qq.com>
2023-05-03 09:51:34 +0800

    20230503/003

--- src/main/java/com/cqq/controller/UserController.java
+++ src/main/java/com/cqq/controller/UserController.java
@@ -2,6 +2,8 @@ package com.cqq.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLOutput;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -9,5 +11,9 @@ public class UserController {
 //    @GetMapping("/{id}")
 //    public Result getById(@PathVariable "{id}"){
 //        return null;
-//    }
+
+    public static void main(String[] args) {
+        System.out.println("hello git~");
+
+    }
 }
